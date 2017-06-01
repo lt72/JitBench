f16e7331cf704ff2cadff530246456f5c94beabe
Juan Sebastian Hoyos Ayala <t-juhoy@microsoft.com>
2017-06-01 11:21:40 -0700

    Fetch nuget and restore tracing packages automated

--- analyze_etw_data.cmd
+++ analyze_etw_data.cmd
@@ -3,6 +3,9 @@
 set BASEDIR=%~dp0%
 set CONFIGURATION=Release
 
+powershell wget https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -O nuget.exe
+nuget restore
+
 pushd src\ETWLogAnalyzer
 devenv ETWLogAnalyzer.csproj /Build %CONFIGURATION%
 bin\%CONFIGURATION%\MusicStore.ETWLogAnalyzer /put=dotnet /etwLog=%BASEDIR%\src\MusicStore\bin\Release\netcoreapp2.0\publish\PerfViewData.etl
