17a4b6c117d2aa4ba89cb111ceb9e6eb733f7010
Juan Sebastian Hoyos Ayala <t-juhoy@microsoft.com>
2017-07-26 15:10:12 -0700

    Clean analysis script

--- Scripts/analyze_etw_data.cmd
+++ Scripts/analyze_etw_data.cmd
@@ -1,18 +1,23 @@
-@echo off
+@if not defined _echo @echo off
 SETLOCAL EnableDelayedExpansion
+
+:: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+:: Do not modify these variables
 set ORIGINALDIR=%CD%
 set SCRIPTDIR=%~dp0
 set BASEDIR=%SCRIPTDIR%..
 set DOTNET=%BASEDIR%\.dotnet\dotnet.exe
 set ETWAPPDIR=%BASEDIR%\ETWLogAnalyzer
 set NUGET=%BASEDIR%\Tools\nuget.exe
-set CONFIGURATION=Release
 set APP=%ETWAPPDIR%\Microsoft.ETWLogAnalyzer.Sample\bin\%CONFIGURATION%\
+:: Modify only these variables as needed.
+set CONFIGURATION=Release
 set REPORTS=%ETWAPPDIR%\Microsoft.ETWLogAnalyzer.Reports\bin\%CONFIGURATION%\
 set ETL=%BASEDIR%\JitBench\src\MusicStore\bin\%CONFIGURATION%\netcoreapp2.0\publish\PerfViewData.etl
-
+set TARGET=dotnet
 set OUT_DIR=E:\Reports
 set WAIT=true
+:: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 cd "%ETWAPPDIR%"
 echo %CD%
@@ -22,7 +27,7 @@ devenv Microsoft.ETWLogAnalyzer.sln /Clean %CONFIGURATION%
 devenv Microsoft.ETWLogAnalyzer.sln /Build %CONFIGURATION%
 
 cd "%APP%"
-Microsoft.ETWLogAnalyzer.Sample.exe /reportGenerators=%REPORTS% /target=dotnet /etwLog=%ETL% /out-dir=%OUT_DIR% /wait=%WAIT%
+Microsoft.ETWLogAnalyzer.Sample.exe /reportGenerators=%REPORTS% /target=%TARGET% /etwLog=%ETL% /out-dir=%OUT_DIR% /wait=%WAIT%
 
 cd %ORIGINALDIR%
 
